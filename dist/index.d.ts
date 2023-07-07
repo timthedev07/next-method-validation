99e4a0ba853d9137de0bda24df441dea8da89757
Tim <harvard90873@gmail.com>
2023-07-07 16:39:47 +0800

    fix

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { NextApiHandler } from "next";
-export type HTTPMethodType = "PUT" | "DELETE" | "POST" | "GET" | "HEAD" | "CONNECT" | "TRADE" | "PATCH" | "OPTIONS";
-export declare const withMethodValidation: (handler: NextApiHandler, method: HTTPMethodType, customMessage?: string, customStatusCode?: number) => NextApiHandler;
-//# sourceMappingURL=index.d.ts.map
